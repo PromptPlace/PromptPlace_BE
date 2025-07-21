ff87e36d14039b4c6e33d52521002fdbb0a98a20
MINJU <mjryu00211@gmail.com>
2025-07-22 00:56:05 +0900

    JWT 토큰을 사용하여 인증된 사용자만 접근 가능하도록 수정

--- src/prompts/routes/prompt.downlaod.route.ts
+++ src/prompts/routes/prompt.downlaod.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { authenticateJwt } from '../../config/passport';
 import { PromptDownloadController } from '../controllers/prompt.download.controller';
 
 const router = Router();
 
-router.get('/:promptId/downloads', PromptDownloadController.getPromptContent);
+router.get('/:promptId/downloads', authenticateJwt, PromptDownloadController.getPromptContent);
 
 export default router;
