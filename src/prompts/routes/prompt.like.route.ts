ed8eae738ffb7988c62b49cac2829380a4ef17e3
MINJU <mjryu00211@gmail.com>
2025-07-22 04:34:53 +0900

    Feat: Jwt 미들웨어 추가

--- src/prompts/routes/prompt.like.route.ts
+++ src/prompts/routes/prompt.like.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { authenticateJwt } from '../../config/passport';
 import { likePrompt, getLikedPrompts } from '../controllers/prompt.like.controller';
 
 const router = Router();
 
-router.post('/:promptId/likes', likePrompt);
-router.get('/likes', getLikedPrompts);
+router.post('/:promptId/likes', authenticateJwt, likePrompt);
+router.get('/likes', authenticateJwt, getLikedPrompts);
 
 export default router;
