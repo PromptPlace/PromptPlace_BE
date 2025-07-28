2c28fcea1d932991e46b016002bcd4ac29b78e43
minij02 <mjryu00211@gmail.com>
2025-07-28 17:57:28 +0900

    Modify: 프론트 도메인 주소 변경사항 반영

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 
 // CORS 설정
-const allowedOrigins = ['https://promptplace-fe.vercel.app'];
+const allowedOrigins = ['https://promptplace-develop.vercel.app'];
 
 app.use(cors({
   origin: allowedOrigins,
