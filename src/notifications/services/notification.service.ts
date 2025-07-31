15b48e2d0d283e8ced117d59fc842127ee39d1ee
Arin0303 <cindy030225@gmail.com>
2025-08-01 01:59:24 +0900

    fix: 알림 취소는 팔로우하지 않아도 가능한 경우로 수정

--- src/notifications/services/notification.service.ts
+++ src/notifications/services/notification.service.ts
@@ -25,25 +25,27 @@ export const createSubscriptionService = async (
         throw new Error('자기 자신은 알림을 받을 수 없습니다.');
     }
 
-    // 팔로우 여부 확인
-    const isFollowing = await findFollowing(userId, prompterId);
-    if (!isFollowing) {
-        throw new Error('프롬프터를 팔로우한 사용자만 알림 설정이 가능합니다.');
-    }
 
     // 기존 알림 설정 확인
     const existingSubscription = await findSubscription(userId, prompterId);
-
     let subscribed: boolean;
-    let subscription;
-    if(existingSubscription){
-        // 이미 알림 설정 -> 알림 설정 취소
-        subscription = await deleteSubscription(userId, prompterId);
-        subscribed = false; // 알림 설정 취소하기
-    } else {
-        // 알림 설정 하지 않은 경우 -> 알림 설정 등록
-        subscription = await createSubscription(userId, prompterId);
-        subscribed = true; // 알림 설정 등록하기
+
+    // 이미 알림 설정 -> 알림 설정 취소
+    if(existingSubscription){ 
+        await deleteSubscription(userId, prompterId);
+        subscribed = false; 
+    } 
+
+    // 알림 설정 하지 않은 경우
+    else { 
+        // 팔로우 여부 확인
+        const isFollowing = await findFollowing(userId, prompterId);
+        if (!isFollowing) { // 팔로우하지 않은 경우 
+            throw new Error('프롬프터를 팔로우한 사용자만 알림 등록이 가능합니다.');
+        } 
+        // 알림 등록
+        await createSubscription(userId, prompterId);
+        subscribed = true; 
     }
 
     return mapToSubscribeResponse(userId, prompterId, subscribed) // 객체로 반환
