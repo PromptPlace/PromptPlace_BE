99b20a5444624684f7fa5707e392a9b81a73032b
minij02 <mjryu00211@gmail.com>
2025-08-05 11:23:42 +0900

    Modify: 유료 프롬프트의 경우, 결제가 완료되었을 경우 다운로드 가능하도록 수정

--- src/prompts/services/prompt.download.service.ts
+++ src/prompts/services/prompt.download.service.ts
@@ -1,6 +1,7 @@
 import { PromptDownloadRepository } from '../repositories/prompt.download.repository';
 import { PromptDownloadResponseDTO } from '../dtos/prompt.download.dto';
 import { AppError } from '../../errors/AppError';
+import prisma from "../../config/prisma";
 
 export const PromptDownloadService = {
   async getPromptContent(userId: number, promptId: number): Promise<PromptDownloadResponseDTO> {
@@ -10,9 +11,25 @@ export const PromptDownloadService = {
       throw new AppError('해당 프롬프트를 찾을 수 없습니다.', 404, 'NotFound');
     }
 
-    // 유료 프롬프트인 경우 다운로드 불가 처리
+    let isPaid = false;
+
+     // 유료 프롬프트인 경우 결제 여부 확인
     if (!prompt.is_free) {
-      throw new AppError('해당 프롬프트는 무료가 아닙니다.', 403, 'Forbidden');
+      const purchase = await prisma.purchase.findFirst({
+        where: {
+          user_id: userId,
+          prompt_id: promptId,
+        },
+        include: {
+          payment: true,
+        },
+      });
+
+      isPaid = purchase?.payment?.status === 'Succeed';
+
+      if (!isPaid) {
+        throw new AppError('해당 프롬프트는 무료가 아니며, 결제가 완료되지 않았습니다.', 403, 'Forbidden');
+      }
     }
 
     return {
@@ -20,6 +37,7 @@ export const PromptDownloadService = {
       title: prompt.title,
       prompt: prompt.prompt,
       is_free: prompt.is_free,
+      is_paid: prompt.is_free ? true : isPaid,
       statusCode: 200,
     };
   }
