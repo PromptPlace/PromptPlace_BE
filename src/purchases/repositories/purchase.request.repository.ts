8178bcf659641cc5b04bd6bc80fcd5d48b19798c
minij02 <mjryu00211@gmail.com>
2025-08-14 03:26:47 +0900

    Feat: 결제 내역 조회 API 구현

--- src/purchases/repositories/purchase.request.repository.ts
+++ src/purchases/repositories/purchase.request.repository.ts
@@ -72,4 +72,28 @@ export const PurchaseRepository = {
       },
     });
   },
+
+  findSucceededByUser(userId: number) {
+    return prisma.purchase.findMany({
+      where: {
+        user_id: userId,
+        payment: { is: { status: 'Succeed'}},
+      },
+      include: {
+        prompt: {
+          select: {
+            prompt_id: true,
+            title: true,
+            user: { select: { nickname: true}},
+          },
+        },
+        payment: {
+          select: {
+            provider: true,
+          },
+        },
+      },
+      orderBy: { created_at: 'desc'},
+    })
+  }
 };
