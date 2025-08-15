8d43ca89d7e2d96841215e5a09a1d91ca49929b0
ggsong0328 <ggsong0328@gmail.com>
2025-08-15 14:19:26 +0900

    fix: 팔로워, 팔로잉 목록 조회 시 팔로워 수 반환 응답 추가

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,6 @@
 import passport from "passport";
-import dotenv from 'dotenv';
-dotenv.config(); 
+import dotenv from "dotenv";
+dotenv.config();
 import {
   Strategy as JwtStrategy,
   ExtractJwt,
