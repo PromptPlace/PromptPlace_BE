f65fbf5c8c94173ba59e1f94f7035b575c0ab685
Arin0303 <cindy030225@gmail.com>
2025-08-15 13:51:14 +0900

    빌드 산출물에 dist 무시

--- dist/purchases/routes/purchase.request.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const passport_1 = require("../../config/passport");
-const purchase_request_controller_1 = require("../controller/purchase.request.controller");
-const router = (0, express_1.Router)();
-router.post('/requests', passport_1.authenticateJwt, purchase_request_controller_1.PurchaseRequestController.createPurchaseRequest);
-exports.default = router;
