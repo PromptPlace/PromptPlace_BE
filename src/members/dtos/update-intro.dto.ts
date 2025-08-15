f651d3810efd6826b8e2b9ab6167cc0cb3bb23ed
ggsong0328 <ggsong0328@gmail.com>
2025-08-15 22:04:38 +0900

    한줄 소개 PATCH 빈 문자열 허용

--- src/members/dtos/update-intro.dto.ts
+++ src/members/dtos/update-intro.dto.ts
@@ -1,9 +1,10 @@
-import { IsString, Length } from "class-validator";
+import { IsOptional, IsString, Length } from "class-validator";
 
 export class UpdateIntroDto {
   @IsString({ message: "한줄 소개는 문자열이어야 합니다." })
-  @Length(1, 100, {
-    message: "한줄 소개는 1자 이상 100자 이하로 입력해주세요.",
+  @IsOptional()
+  @Length(0, 100, {
+    message: "한줄 소개는 100자 이하로 입력해주세요.",
   })
   intro!: string;
 }
