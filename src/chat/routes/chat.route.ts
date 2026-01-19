a3a8a3250a43b9a891e8fe9d582d9d56c10e98f7
Arin0303 <ape4246@gmail.com>
2026-01-19 12:59:53 +0900

    add: 라우터 등록 코드 추가

--- src/chat/routes/chat.route.ts
+++ src/chat/routes/chat.route.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { createOrGetChatRoom } from "../controllers/chat.controller";
 import { authenticateJwt } from "../../config/passport";
 
-
 const router = Router();
 /**
  * @swagger
