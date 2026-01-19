5875e02e27b37e08b8dc5dccb06fe150876aa2bf
Arin0303 <ape4246@gmail.com>
2026-01-19 23:21:36 +0900

    feat: 채팅방 생성 repository를 class 형식으로 변환

--- src/chat/repositories/chat.repository.ts
+++ src/chat/repositories/chat.repository.ts
@@ -1,28 +1,31 @@
+// repositories/chat.repository.ts
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 
-export const findChatRoomByParticipants = async (
-    user_id1: number, user_id2: number
-) => {
-    const existChatRoom = await prisma.chatRoom.findFirst({
-        where: { 
-            user_id1 : user_id1, 
-            user_id2: user_id2 
-        },
+export class ChatRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async findChatRoomByParticipants(
+    user_id1: number,
+    user_id2: number
+  ) {
+    return this.prisma.chatRoom.findFirst({
+      where: {
+        user_id1,
+        user_id2,
+      },
     });
-    return existChatRoom;
-}
+  }
 
-export const createChatRoom = async (
-    user_id1: number, user_id2: number
-) => {  
-    const newChatRoom = await prisma.chatRoom.create({
-        data: {
-            user_id1 : user_id1, 
-            user_id2: user_id2,
-            last_message_id: null,
-        },
+  async createChatRoom(
+    user_id1: number,
+    user_id2: number
+  ) {
+    return this.prisma.chatRoom.create({
+      data: {
+        user_id1,
+        user_id2,
+        last_message_id: null,
+      },
     });
-    return newChatRoom;
+  }
 }
-
