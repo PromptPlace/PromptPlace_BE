170947c22ec2351069701387da47a5a80c0f9224
Arin0303 <ape4246@gmail.com>
2026-01-20 13:15:19 +0900

    fix: 기존 service계층을 class 형태로 리팩토링

--- src/chat/services/chat.service.ts
+++ src/chat/services/chat.service.ts
@@ -1,31 +1,33 @@
-import { 
-  findChatRoomByParticipants, 
-  createChatRoom,
-} from "../repositories/chat.repository";
+import { Service } from "typedi";
+import { ChatRepository } from "../repositories/chat.repository";
 
 import { 
   ChatRoomResponseDto 
 } from "../dtos/chat.dto";
 
-export const createOrGetChatRoomService = async (
+@Service()
+export class ChatService {
+  constructor(private readonly chatRepo: ChatRepository) {}
+  async createOrGetChatRoomService(
     userId: number, partnerId: number
-): Promise<ChatRoomResponseDto> => {
-  const userId1 = Math.min(userId, partnerId);
-  const userId2 = Math.max(userId, partnerId);
+  ): Promise<ChatRoomResponseDto> {
+    const userId1 = Math.min(userId, partnerId);
+    const userId2 = Math.max(userId, partnerId);
 
-  const existingRoom = await findChatRoomByParticipants(userId1, userId2);
-  
-  if (existingRoom) { // 존재하는 채팅방이 있으면 반환
+    const existingRoom = await this.chatRepo.findChatRoomByParticipants(userId1, userId2);
+    
+    if (existingRoom) { // 존재하는 채팅방이 있으면 반환
+      return {
+        room_id: existingRoom.room_id,
+        is_new: false,
+      };
+    }
+
+    // 없으면 새로 생성
+    const newRoom = await this.chatRepo.createChatRoom(userId1, userId2);
     return {
-      room_id: existingRoom.room_id,
-      is_new: false,
+      room_id: newRoom.room_id,
+      is_new: true,
     };
   }
-
-  // 없으면 새로 생성
-  const newRoom = await createChatRoom(userId1, userId2);
-  return {
-    room_id: newRoom.room_id,
-    is_new: true,
-  };
 }
